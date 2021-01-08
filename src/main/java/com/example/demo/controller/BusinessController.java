9d1bdadf889f59aae084c3506dadf65673574902
van <vanpersieyolo@gmail.com>
2021-01-08 17:12:58 +0700

    lam chuyen vl

--- src/main/java/com/example/demo/controller/BusinessController.java
+++ src/main/java/com/example/demo/controller/BusinessController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/book")
 public class BusinessController {
+
 }
